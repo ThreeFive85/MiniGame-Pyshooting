38260c3335ea580bc6f8f172244f6ce988a8ab5a
ThreeFive85 <kaje0god@naver.com>
2020-05-25 22:13:27 +0900

    Fix : 게임 배경화면 구성  - png 파일을 불러와 검은 화면에서 새로운 백그라운드 화면으로 업데이트

--- game.py
+++ game.py
@@ -2,21 +2,29 @@ import pygame
 import sys
 from time import sleep
 
-BLACK = (0, 0, 0)
+# BLACK = (0, 0, 0)
 padWidth = 480
 padHeight = 640
 
+# 배경화면 그리는 drawObject 함수
+
+
+def drawObject(obj, x, y):
+    global gamePad
+    gamePad.blit(obj, (x, y))
+
 
 def initGame():
-    global gamePad, clock
+    global gamePad, clock, background
     pygame.init()
     gamePad = pygame.display.set_mode((padWidth, padHeight))
     pygame.display.set_caption('PyShooting')
+    background = pygame.image.load('./resource/background.png')
     clock = pygame.time.Clock()
 
 
 def runGame():
-    global gapdPad, clock
+    global gamePad, clock, background
 
     onGame = False
     while not onGame:
@@ -25,7 +33,8 @@ def runGame():
                 pygame.quit()
                 sys.exit()
 
-        gamePad.fill(BLACK)
+        # gamePad.fill(BLACK)
+        drawObject(background, 0, 0)
 
         pygame.display.update()
 
