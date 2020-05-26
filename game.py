84c696e832e6c014439d9508d4099b25c2024cf0
ThreeFive85 <kaje0god@naver.com>
2020-05-26 20:47:13 +0900

    Fix : 화면에 전투기 표시  - 전투기그림파일을 불러와 초기 화면 아래 중앙에 표시  - 폴더 안에 있는 파일들을 불러오지 못해서 일단 파일의 전체 경로를    지정해주는 방법으로 해결

--- game.py
+++ game.py
@@ -5,26 +5,36 @@ from time import sleep
 # BLACK = (0, 0, 0)
 padWidth = 480
 padHeight = 640
+# 이미지를 그냥 ./resource/background.png로 해도 되었는데 또 이상하게 안되서
+# 그냥 전체리소스 경로를 지정해 놓음
+resourcePath = '/home/donghyuk/바탕화면/Python/PyShooting/resource/'
 
-# 배경화면 그리는 drawObject 함수
 
-
-def drawObject(obj, x, y):
+def drawObject(obj, x, y):  # 배경화면 그리는 drawObject 함수
     global gamePad
     gamePad.blit(obj, (x, y))
 
 
 def initGame():
-    global gamePad, clock, background
+    global gamePad, clock, background, fighter
     pygame.init()
     gamePad = pygame.display.set_mode((padWidth, padHeight))
     pygame.display.set_caption('PyShooting')
-    background = pygame.image.load('./resource/background.png')
+    background = pygame.image.load(resourcePath+'background.png')
+    fighter = pygame.image.load(resourcePath+'fighter.png')
     clock = pygame.time.Clock()
 
 
 def runGame():
-    global gamePad, clock, background
+    global gamePad, clock, background, fighter
+
+    fighterSize = fighter.get_rect().size  # 전투기 사이즈
+    fighterWidth = fighterSize[0]
+    fighterHeight = fighterSize[1]
+
+    x = padWidth * 0.45  # 전투기 초기 위치
+    y = padHeight * 0.9
+    fighterX = 0
 
     onGame = False
     while not onGame:
@@ -35,6 +45,8 @@ def runGame():
 
         # gamePad.fill(BLACK)
         drawObject(background, 0, 0)
+
+        drawObject(fighter, x, y)  # 전투기 표시
 
         pygame.display.update()
 
