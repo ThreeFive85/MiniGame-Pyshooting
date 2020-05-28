ae939c576f3b1b2e10bdd12d47498d48a2747bf0
ThreeFive85 <kaje0god@naver.com>
2020-05-28 14:55:04 +0900

    Fix : 미사일 발사 코드 작성  - 화면에 미사일이 발사되게끔 코드 작성

--- game.py
+++ game.py
@@ -16,17 +16,18 @@ def drawObject(obj, x, y):  # 배경화면 그리는 drawObject 함수
 
 
 def initGame():
-    global gamePad, clock, background, fighter
+    global gamePad, clock, background, fighter, missile
     pygame.init()
     gamePad = pygame.display.set_mode((padWidth, padHeight))
     pygame.display.set_caption('PyShooting')
     background = pygame.image.load(resourcePath+'background.png')
     fighter = pygame.image.load(resourcePath+'fighter.png')
+    missile = pygame.image.load(resourcePath+'missile.png')
     clock = pygame.time.Clock()
 
 
 def runGame():
-    global gamePad, clock, background, fighter
+    global gamePad, clock, background, fighter, missile
 
     fighterSize = fighter.get_rect().size  # 전투기 사이즈
     fighterWidth = fighterSize[0]
@@ -35,6 +36,8 @@ def runGame():
     x = padWidth * 0.45  # 전투기 초기 위치
     y = padHeight * 0.9
     fighterX = 0
+
+    missileXY = []
 
     onGame = False
     while not onGame:
@@ -50,6 +53,11 @@ def runGame():
                 elif event.key == pygame.K_RIGHT:
                     fighterX += 5
 
+                elif event.key == pygame.K_SPACE:  # 스페이스바를 누르면 미사일 발사
+                    missileX = x + fighterWidth/2
+                    missileY = y - fighterHeight
+                    missileXY.append([missileX, missileY])
+
             if event.type in [pygame.KEYUP]:  # 왼쪽,오른쪽 키보드를 떘을때
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                     fighterX = 0
@@ -64,6 +72,22 @@ def runGame():
             x = padWidth - fighterWidth
 
         drawObject(fighter, x, y)  # 전투기 표시
+
+        if len(missileXY) != 0:
+            # enumerate로 missileXY에 대해 전체적으로 돌고
+            for i, bxy in enumerate(missileXY):
+                bxy[1] -= 10  # 미사일은 위로 발사되기깨문에 y좌표에서 -10씩
+                missileXY[i][1] = bxy[1]  # 미사일은 여러개이기때문에 bxy에 대한 값을 넣어 준다.
+
+                if bxy[1] <= 0:  # 미사일이 화면 밖으로 넘어 갔을때
+                    try:
+                        missileXY.remove(bxy)
+                    except:
+                        pass
+
+        if len(missileXY) != 0:  # 미사일 실제 그리기
+            for bx, by in missileXY:
+                drawObject(missile, bx, by)
 
         pygame.display.update()
 
